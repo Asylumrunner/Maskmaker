175fd5a4a11e469b700e6270fd44bf78b7856787
Asylumrunner <asylumrunner@gmail.com>
2019-09-16 19:28:09 -0500

    Enable health endpoint

--- setup/routes.js
+++ setup/routes.js
@@ -2,11 +2,11 @@ const winston = require('winston');
 const express = require('express');
 //const auth = require('../routes/auth');
 const npcs = require('../routes/npcs');
-//const health = require('../routes/health');
+const health = require('../routes/health');
 
 module.exports = function(app) {
     app.use(express.json());
     //app.use('/api/auth', auth);
     app.use('/api/npcs/', npcs);
-    //app.use('/api/health/', health);
+    app.use('/api/health/', health);
 }
